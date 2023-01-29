b2ce508c70fb5895d978e5335da21fc0944bf530
kriti <kritijulia@gmail.com>
2023-01-29 22:11:28 +0530

    first programming in C

---
+++ C001.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+float main(){
+    float celsius;
+    float fahrenheit;
+    celsius=55;
+    fahrenheit = (((9*celsius)/5) + 32);
+    printf("temprature in fahrenheit is %f\n" , fahrenheit);
+    printf("temperature in celsius is %f\n" , celsius);
+}
