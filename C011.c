1f028074fb148ae834ee7c7da80cdcbffa20e136
kriti <kritijulia@gmail.com>
2023-02-01 21:12:46 +0530

    diff in do while and while loop

---
+++ C011.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{
+   int a;
+   do{
+   	printf(" you typed : ");
+    scanf("%d",&a);
+   } while(a != -1);
+}
+//#include<stdio.h>
+//int main ()
+//{
+//	int b;
+//	while(a!=-1){
+//	printf(" you typed : ");
+//    scanf("%d",&b);
+//	}
+//}
